e1f8e3f280fc1c38c51e3ed8b0e8a819a35a098d
Rita <94860684+ritullia@users.noreply.github.com>
2022-02-22 22:07:40 +0200

    css style update

--- doc.js
+++ doc.js
@@ -1,22 +1,22 @@
-let img = document.getElementById("girl")
+// let img = document.getElementById("girl")
 
-img.addEventListener("mouseenter", mouseEnter);
-img.addEventListener("mouseleave", mouseLeave);
+// img.addEventListener("mouseenter", mouseEnter);
+// img.addEventListener("mouseleave", mouseLeave);
 
-function mouseEnter() {
-    document.getElementById("girl").style.height = "250px"
-    document.getElementById("girl").style.width = "300px"
-};
-function mouseLeave() {
-    document.getElementById("girl").style.height = "150px"
-    document.getElementById("girl").style.width = "200px"
-};
+// function mouseEnter() {
+//     document.getElementById("girl").style.height = "250px"
+//     document.getElementById("girl").style.width = "300px"
+// };
+// function mouseLeave() {
+//     document.getElementById("girl").style.height = "100px"
+//     document.getElementById("girl").style.width = "200px"
+// };
 
 
-window.addEventListener("resize", sizeWindow);
+// window.addEventListener("resize", sizeWindow);
 
-let x = 0;
-function sizeWindow() {
-    let txt = x += 1;
-    document.getElementById("size").innerHTML = txt;
-};
+// let x = 0;
+// function sizeWindow() {
+//     let txt = x += 1;
+//     document.getElementById("size").innerHTML = txt;
+// };
